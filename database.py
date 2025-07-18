38ecf2a64c2b0d82f7947c4174747b487e6420b4
Niharika Kethepalli <knss1710@gmail.com>
2025-07-18 19:17:43 +0530

    Trigger redeploy after DATABASE_URL set

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
+import os
+
+DATABASE_URL = os.getenv("postgresql://postgres:rZeiafNLJxTNpZQlrinOfcAWnnFzQuOh@metro.proxy.rlwy.net:29641/railway")
+print("DATABASE_URL:", DATABASE_URL)  # Add this line
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-
-DATABASE_URL = os.environ.get("postgresql://postgres:rZeiafNLJxTNpZQlrinOfcAWnnFzQuOh@metro.proxy.rlwy.net:29641/railway")
 
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
