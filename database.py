469e8131d262fd2fbf24298644ad2102cbbe90aa
Niharika Kethepalli <knss1710@gmail.com>
2025-07-20 23:16:41 +0530

    Clean working FastAPI project with correct DATABASE_URL handling

--- database.py
+++ database.py
@@ -3,7 +3,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+# Get the DATABASE_URL from Railway environment variables
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
