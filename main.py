f081f80953d1a0b7927fe4437229cd589fbb63f2
Niharika Kethepalli <knss1710@gmail.com>
2025-07-21 17:25:40 +0530

    Updated POST API for nested fields structure

--- main.py
+++ main.py
@@ -43,3 +43,7 @@ def create_wheel_specification(
         "submittedBy": spec.submittedBy,
         "submittedDate": spec.submittedDate
     }}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
